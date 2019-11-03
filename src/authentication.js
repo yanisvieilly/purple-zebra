b35889fdaafde5c0ca21e7e6695e3e5ca1d37ee2
Yanis Vieilly <yanis.vieilly+github@protonmail.com>
2019-11-03 18:31:10 +0100

    Add request interceptor with authentication token

--- src/authentication.js
+++ src/authentication.js
@@ -1,16 +1,24 @@
+import axios from "axios";
+
 export const authenticate = async () => {
-  const response = await fetch("https://edge.blablacar.com/token", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
+  const { data } = await axios.post(
+    "https://edge.blablacar.com/token",
+    {
       grant_type: "client_credentials",
       client_id: "dummy-client",
       client_secret: "UizxdvqmPxhO7s3IPqy9C5bkmTU9B0zL",
       scopes: ["SCOPE_TRIP_DRIVER", "DEFAULT", "SCOPE_INTERNAL_CLIENT"]
-    })
+    },
+    {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
+
+  axios.interceptors.request.use(config => {
+    config.headers.Authorization = `Bearer ${data.access_token}`;
   });
 
-  return response.json();
+  return data;
 };
