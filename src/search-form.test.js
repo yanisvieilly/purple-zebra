606992b7ab6a1064610e9170984f496201b5b955
Yanis Vieilly <yanis.vieilly+github@protonmail.com>
2019-11-04 17:09:34 +0100

    Add tests for SearchForm

--- src/search-form.test.js
+++ src/search-form.test.js
@@ -2,16 +2,53 @@ import React from "react";
 import { shallow } from "enzyme";
 
 import SearchForm from "./search-form";
+import CitySelector from "./city-selector";
+
+const FROM_CITY = "Paris";
+const TO_CITY = "Marseille";
 
 describe("SearchForm test suite", () => {
-  it("renders without crashing", () => {
-    shallow(
+  let component;
+
+  beforeEach(() => {
+    component = (
       <SearchForm
-        fromCity="Paris"
-        toCity="Marseille"
+        fromCity={FROM_CITY}
+        toCity={TO_CITY}
         handleFromCityChange={() => {}}
         handleToCityChange={() => {}}
       />
     );
+  });
+
+  it("renders without crashing", () => {
+    shallow(component);
+  });
+
+  it("renders two CitySelector components", () => {
+    const wrapper = shallow(component);
+    const citySelectors = wrapper.find(CitySelector);
+
+    expect(citySelectors.length).toEqual(2);
+  });
+
+  it("sets the from CitySelector value", () => {
+    const wrapper = shallow(component);
+    const fromCity = wrapper
+      .find(CitySelector)
+      .first()
+      .prop("value");
+
+    expect(fromCity).toEqual(FROM_CITY);
+  });
+
+  it("sets the from CitySelector value", () => {
+    const wrapper = shallow(component);
+    const toCity = wrapper
+      .find(CitySelector)
+      .last()
+      .prop("value");
+
+    expect(toCity).toEqual(TO_CITY);
   });
 });
